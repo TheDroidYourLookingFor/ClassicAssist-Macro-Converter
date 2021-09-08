f6bd029ba5ce639a8c06c3506ae94cff97369969
TheDroidYourLookingFor <90306870+TheDroidYourLookingFor@users.noreply.github.com>
2021-09-08 01:54:16 -0500

    Proof of concept
    First draft that will convert basic uosteam macros

---
+++ ClassicAssist Macro Converter/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace ClassicAssist_Macro_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.parseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(1, 56);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.richTextBox1.Size = new System.Drawing.Size(499, 488);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.Location = new System.Drawing.Point(499, 56);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.richTextBox2.Size = new System.Drawing.Size(499, 488);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(137, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Current File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(691, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Modified File";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.parseToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1000, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // parseToolStripMenuItem
+            // 
+            this.parseToolStripMenuItem.ForeColor = System.Drawing.Color.Red;
+            this.parseToolStripMenuItem.Name = "parseToolStripMenuItem";
+            this.parseToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.parseToolStripMenuItem.Text = "Parse";
+            this.parseToolStripMenuItem.Click += new System.EventHandler(this.parseToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1000, 548);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "ClassicAssist Macro Converter";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem parseToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ ClassicAssist Macro Converter/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace ClassicAssist_Macro_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "UOSteam Files (*.uos)|*.uos";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFileName = openFileDialog1.FileName;
+                //...
+                richTextBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "*.macro|*.macro";
+            saveFileDialog1.RestoreDirectory = true;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, richTextBox2.Text);
+            }
+        }
+
+        private void parseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text = richTextBox1.Text.Replace("endif", "");
+            richTextBox2.Text = richTextBox2.Text.Replace("endwhile", "");
+            richTextBox2.Text = richTextBox2.Text.Replace("maxmana", "MaxMana()");
+            richTextBox2.Text = richTextBox2.Text.Replace("maxhits", "MaxHits()");
+            richTextBox2.Text = richTextBox2.Text.Replace("mana", "Mana()");
+            richTextBox2.Text = richTextBox2.Text.Replace("hits", "Hits()");
+            richTextBox2.Text = richTextBox2.Text.Replace("@", "");
+            richTextBox2.Text = richTextBox2.Text.Replace("!", "");
+            richTextBox2.Text = richTextBox2.Text.Replace("//", "#");
+            richTextBox2.Text = richTextBox2.Text.Replace("stop", "Stop()");
+            richTextBox2.Text = richTextBox2.Text.Replace("grabitem", "GrabItem()");
+            richTextBox2.Text = richTextBox2.Text.Replace("fly", "Fly()");
+            richTextBox2.Text = richTextBox2.Text.Replace("land", "Land()");
+            richTextBox2.Text = richTextBox2.Text.Replace("bandageself", "BandageSelf()");
+            richTextBox2.Text = richTextBox2.Text.Replace("resync", "Resync()");
+            richTextBox2.Text = richTextBox2.Text.Replace("ping", "Ping()");
+            richTextBox2.Text = richTextBox2.Text.Replace("hotkeys", "Hotkeys()");
+            richTextBox2.Text = richTextBox2.Text.Replace("info", "Info()");
+            richTextBox2.Text = richTextBox2.Text.Replace("clearjournal", "ClearJournal()");
+            richTextBox2.Text = richTextBox2.Text.Replace("else\n", "else:\n");
+            richTextBox2.Text = richTextBox2.Text.Replace("elseif", "elif");
+
+            //FindObject
+            Regex regex = new Regex("findobject '(.*)'", RegexOptions.IgnoreCase);
+            string cleanString = regex.Replace(richTextBox2.Text, "FindObject($1)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("playmacro (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "PlayMacro($1)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("messagebox (.*) (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "MessageBox($1, $2)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("playsound (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "PlaySound($1)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("clearhands (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "ClearHands($1)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("feed (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "Feed($1)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("clickobject (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "ClickObject($1)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("attack (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "Attack($1)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("bandage (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "Bandage($1)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("snapshot (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "Snapshot($1)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("useobject (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "UseObject($1)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("setability (.*) (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "SetAbility($1, $2)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("setability (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "SetAbility($1)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("findtype (.*) (.*) (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "FindType($1, $2, $3)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("usetype (.*) (.*) (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "UseType($1, $2, $3)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("movetype (.*) (.*) (.*) (.*) (.*) (.*) (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "MoveType($1, $2, $3)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("movetype (.*) (.*) (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "MoveType($1, $2, $3)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("equipitem (.*) (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "EquipItem($1, $2)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("sysmsg (.*) (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "SysMsg($1, $2)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("headmsg (.*) (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "HeadMsg($1, $2)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("injournal (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "InJournal($1)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("autotargetobject (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "AutoTargetObject($1)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("waitfortarget (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "WaitForTarget($1)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("targettileoffset (.*) (.*) (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "TargetTileOffset($1, $2, $3)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("pause (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "Pause($1)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("promptalias '(.*)'", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "PromptAlias('$1')");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("target (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "Target($1)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("skill '(.*)' >", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "Skill('$1') >");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("skill '(.*)' <", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "Skill('$1') <");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("skill '(.*)' =", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "Skill('$1') =");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("skill '(.*)' !", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "Skill('$1') !");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("moveitem (.*) (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "MoveItem($1, $2)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("waitforgump (.*) (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "WaitForGump($1, $2)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("replygump (.*) (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "ReplyGump($1, $2)");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("if (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "if $1:");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("cast '(.*)' '(.*)'", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "Cast('$1', '$2')");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("cast '(.*)'", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "Cast('$1')");
+            richTextBox2.Text = cleanString;
+
+            regex = new Regex("buffexists (.*)", RegexOptions.IgnoreCase);
+            cleanString = regex.Replace(richTextBox2.Text, "BuffExists($1)");
+            richTextBox2.Text = cleanString;
+        }
+    }
+}
